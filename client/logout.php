494f419db386a8dac7d0e15df58fe36fc90dc16f
byProjecthub <njabulod.hlongwane@gmail.com>
2026-07-20 18:56:47 +0200

    Delete client directory

--- client/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once '../includes/functions.php';
-session_start();
-unset($_SESSION['client_id'], $_SESSION['client_name'], $_SESSION['client_email']);
-session_destroy();
-redirect('login.php');
