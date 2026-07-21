a570d82e20ed7e281de80ae945877f1fafd28f0c
byProjecthub <njabulod.hlongwane@gmail.com>
2026-07-21 11:33:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-// Uncomment the next two lines to see errors, then comment them back once fixed
-// error_reporting(E_ALL);
-// ini_set('display_errors', '1');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 declare(strict_types=1);
 $pageTitle = 'Home';
